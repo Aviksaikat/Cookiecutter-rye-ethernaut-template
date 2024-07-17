b0c82072ada5009988cfb7d7eeb8e051a24c8c0e
Aviksaikat <saikickkarma@gmail.com>
2024-07-17 23:55:39 +0530

    feat: simple template created

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,31 @@
 #!/usr/bin/python3
 import subprocess
 
+def install_rye():
+    # First part of the command
+    curl_command = ["curl", "-sSf", "https://rye.astral.sh/get"]
+
+    # Second part of the command
+    bash_command = ["bash"]
+
+    # Run the curl command and pipe its output to the bash command
+    curl_process = subprocess.Popen(curl_command, stdout=subprocess.PIPE)
+    bash_process = subprocess.run(bash_command, stdin=curl_process.stdout, capture_output=True, text=True)
+
+    # Close the stdout pipe of curl_process to signal that no more data will be written
+    curl_process.stdout.close()
+
+    # Wait for curl_process to finish
+    curl_process.wait()
+
+    # Print the output and errors
+    # print(bash_process.stdout)
+    if bash_process.stderr:
+        print(bash_process.stderr)
+
+
 try:
-    subprocess.call(["curl", "-sSf", "https://rye.astral.sh/get", "|", "bash"])
+    install_rye()
     subprocess.call(["rye", "init"])
     subprocess.call(["rye", "add", "eth-ape", "--pre"])
     subprocess.call(["rye", "sync", "--pre"])
